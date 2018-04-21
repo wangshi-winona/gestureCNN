f089d003c5ab32c7b28d86d1b039d7b11f8b1194
Winona Shi WANG <winonaswang@outlook.com>
2018-04-21 22:19:09 +0800

    start code

---
+++ detect.py
@@ -0,0 +1,238 @@
+import cv2
+import mymodel as myNN
+import json
+
+import time
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+
+
+
+minValue = 70
+
+x0 = 400
+y0 = 200
+height = 200
+width = 200
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+size = 1.0
+fx = 20
+fy = 500
+fh = 50
+
+isGuessing = False
+
+weightfile = "newWeight.hdf5"
+
+def binaryMask(frame, x0, y0, width, height):
+    global isGuessing, mod
+
+    cv2.rectangle(frame, (x0, y0), (x0 + width, y0 + height), (0, 255, 0), 1)
+    roi = frame[y0:y0 + height, x0:x0 + width]
+
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 2)
+    # blur = cv2.bilateralFilter(roi,9,75,75)
+
+    th3 = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    ret, res = cv2.threshold(th3, minValue, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    # ret, res = cv2.threshold(blur, minValue, 255, cv2.THRESH_BINARY +cv2.THRESH_OTSU)
+
+    # if saveImg == True:
+    #     saveROIImg(res)
+    if isGuessing == True:
+        retgesture = guessGesture(mod, res)
+        cv2.putText(frame, myNN.output[int(retgesture)], (fx, fy + fh), font, size*3, (0, 255, 0), 1, 1)
+        # if lastgesture != retgesture:
+        #     lastgesture = retgesture
+        #     print lastgesture
+
+            #
+            #     ## Checking for only PUNCH gesture here
+            #     ## Run this app in Prediction Mode and keep Chrome browser on focus with Internet Off
+            #     ## And have fun :) with Dino
+            #     if lastgesture == 3:
+            #         jump = ''' osascript -e 'tell application "System Events" to key code 49' '''
+            #         #jump = ''' osascript -e 'tell application "System Events" to key down (49)' '''
+            #         os.system(jump)
+            #         print myNN.output[lastgesture] + "= Dino JUMP!"
+            #
+        time.sleep(0.1)
+            #     #guessGesture = False
+    # elif visualize == True:
+    #     layer = int(raw_input("Enter which layer to visualize "))
+    #     cv2.waitKey(1)
+    #     myNN.visualizeLayers(mod, res, layer)
+    #     visualize = False
+
+    return res
+
+# def loadModel():
+#     global get_output
+#     model = Sequential()
+#
+#     model.add(Conv2D(myNN.nb_filters, (myNN.nb_conv, myNN.nb_conv),
+#                      padding='valid', data_format = 'channels_first',
+#                      input_shape=(myNN.img_channels, myNN.img_rows, myNN.img_cols)))
+#     convout1 = Activation('relu')
+#     model.add(convout1)
+#     model.add(Conv2D(myNN.nb_filters, (myNN.nb_conv, myNN.nb_conv)))
+#     convout2 = Activation('relu')
+#     model.add(convout2)
+#     model.add(MaxPooling2D(pool_size=(myNN.nb_pool, myNN.nb_pool)))
+#     model.add(Dropout(0.5))
+#
+#     model.add(Flatten())
+#     model.add(Dense(128))
+#     model.add(Activation('relu'))
+#     model.add(Dropout(0.5))
+#     model.add(Dense(myNN.nb_classes))
+#     model.add(Activation('softmax'))
+#
+#
+#     # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+#     model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+#
+#     # Model summary
+#     model.summary()
+#     # Model conig details
+#     model.get_config()
+#
+#
+#     print "loading ", weightfile
+#     model.load_weights(weightfile)
+#
+#     layer = model.layers[11]
+#     get_output = K.function([model.layers[0].input, K.learning_phase()], [layer.output, ])
+#
+#     return model
+
+
+# This function does the guessing work based on input images
+def guessGesture(model, img):
+    global get_output
+
+
+    # Load image and flatten it
+    image = np.array(img).flatten()
+
+    # reshape it
+    image = image.reshape(myNN.img_channels, myNN.img_rows, myNN.img_cols)
+
+    # float32
+    image = image.astype('float32')
+
+    # normalize it
+    image = image / 255
+
+    # reshape for NN
+    rimage = image.reshape(1, myNN.img_channels, myNN.img_rows, myNN.img_cols)
+
+    # Now feed it to the NN, to fetch the predictions
+    # index = model.predict_classes(rimage)
+    # prob_array = model.predict_proba(rimage)
+
+    prob_array = get_output([rimage, 0])[0]
+
+    # print prob_array
+
+    d = {}
+    i = 0
+    for items in myNN.output:
+        d[items] = (int)(prob_array[0][i] * 100)
+        i += 1
+
+    # Get the output with maximum probability
+    import operator
+
+    guess = max(d.iteritems(), key=operator.itemgetter(1))[0]
+    prob = d[guess]
+    # print d
+
+    if prob > 80.0:
+        print guess + "  Probability: ", prob
+        # print myNN.output.index(guess)
+        # Enable this to save the predictions in a json file,
+        # Which can be read by plotter app to plot bar graph
+        # dump to the JSON contents to the file
+
+        # with open('gesturejson.txt', 'w') as outfile:
+        #     json.dump(d, outfile)
+
+        return myNN.output.index(guess)
+
+    else:
+        return 1
+
+def Main():
+    global get_output, isGuessing, visualize, mod, x0, y0, width, height
+
+    mod, get_output = myNN.loadCNN(0, weightfile)
+
+    ## Grab camera input
+    cap = cv2.VideoCapture(0)
+    cv2.namedWindow('Original', cv2.WINDOW_AUTOSIZE)
+
+    # set rt size as 640x480
+    ret = cap.set(3, 640)
+    ret = cap.set(4, 480)
+
+    while (True):
+        ret, frame = cap.read()
+        max_area = 0
+
+        frame = cv2.flip(frame, 3)
+
+        if ret == True:
+            # if binaryMode == True:
+            roi = binaryMask(frame, x0, y0, width, height)
+            # else:
+            #     roi = skinMask(frame, x0, y0, width, height)
+
+        # cv2.putText(frame, 'Options:', (fx, fy), font, 0.7, (0, 255, 0), 2, 1)
+        # cv2.putText(frame, 'b - Toggle Binary/SkinMask', (fx, fy + fh), font, size, (0, 255, 0), 1, 1)
+        cv2.putText(frame, 'g - Toggle Prediction Mode', (fx, fy + 2 * fh), font, size, (0, 255, 0), 1, 1)
+        # cv2.putText(frame, 'q - Toggle Quiet Mode', (fx, fy + 3 * fh), font, size, (0, 255, 0), 1, 1)
+        # cv2.putText(frame, 'n - To enter name of new gesture folder', (fx, fy + 4 * fh), font, size, (0, 255, 0), 1, 1)
+        # cv2.putText(frame, 's - To start capturing new gestures for training', (fx, fy + 5 * fh), font, size,
+        #             (0, 255, 0), 1, 1)
+        cv2.putText(frame, 'ESC - Exit', (fx, fy + 3 * fh), font, size, (0, 255, 0), 1, 1)
+
+        cv2.imshow('Original', frame)
+        cv2.imshow('ROI', roi)
+
+        # Keyboard inputs
+        key = cv2.waitKey(10) & 0xff
+
+        ## Use Esc key to close the program
+        if key == 27:
+            break
+
+        ## Use g key to start gesture predictions via CNN
+        elif key == ord('g'):
+            isGuessing = not isGuessing
+            print "Prediction Mode - {}".format(isGuessing)
+
+
+        ## Use i,j,k,l to adjust ROI window
+        elif key == ord('i'):
+            y0 = y0 - 5
+        elif key == ord('k'):
+            y0 = y0 + 5
+        elif key == ord('j'):
+            x0 = x0 - 5
+        elif key == ord('l'):
+            x0 = x0 + 5
+
+
+    # Realse & destroy
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    Main()
---
+++ mymodel.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.utils import np_utils
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from PIL import Image
+import numpy as np
+import os
+
+
+# We require this for Theano lib ONLY. Remove it for TensorFlow usage
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+nb_filters=32
+nb_conv=3
+img_channels=1
+img_rows=200
+img_cols=200
+nb_pool=2
+nb_classes=4
+batch_size=30
+nb_epoch=3
+defaultfname="myWeights.hdf5"
+path = "./"
+path1 = "./gestures"    #path of folder of images
+
+## Path2 is the folder which is fed in to training model
+path3 = './imgfolder_b'
+
+path2="./mytrain2"##for our trainning
+
+# outputs
+output = [ "SCISSORS","NOTHING","ROCK", "PAPER"]
+
+
+
+#%%
+def modlistdir(path):
+    listing = os.listdir(path)
+    retlist = []
+    for name in listing:
+        #This check is to ignore any hidden files/folders
+        if name.startswith('.'):
+            continue
+        retlist.append(name)
+    return retlist
+
+def loadCNN(wf_index, fname):
+    global get_output
+    model = Sequential()
+
+    model.add(Conv2D(nb_filters, (nb_conv, nb_conv),
+                     padding='valid', data_format = 'channels_first',
+
+                     input_shape=(img_channels, img_rows, img_cols)))
+    convout1 = Activation('relu')
+    model.add(convout1)
+    model.add(Conv2D(nb_filters, (nb_conv, nb_conv)))
+    convout2 = Activation('relu')
+    model.add(convout2)
+    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+    # Model summary
+    model.summary()
+    # Model conig details
+    model.get_config()
+
+    if wf_index >= 0:
+        # Load pretrained weights
+        print "loading ", fname
+        model.load_weights(fname)
+
+    print len(model.layers)
+    layer = model.layers[11]
+    get_output = K.function([model.layers[0].input, K.learning_phase()], [layer.output, ])
+
+    return model, get_output
+
+
+def initializers():
+    imlist = modlistdir(path2)
+
+    image1 = np.array(Image.open(path2 + '/' + imlist[0]))  # open one image to get size
+    # plt.imshow(im1)
+
+    m, n = image1.shape[0:2]  # get the size of the images
+    total_images = len(imlist)  # get the 'total' number of images
+
+    # create matrix to store all flattened images
+    immatrix = np.array([np.array(Image.open(path2 + '/' + images).convert('L')).flatten()
+                         for images in imlist], dtype='f')
+
+    print immatrix.shape
+
+    raw_input("Press any key")
+
+    #########################################################
+    ## Label the set of images per respective gesture type.
+    ##
+    label = np.ones((total_images,), dtype=int)
+
+    samples_per_class = total_images / nb_classes
+    print "samples_per_class - ", samples_per_class
+    s = 0
+    r = samples_per_class
+    for classIndex in range(nb_classes):
+        label[s:r] = classIndex
+        s = r
+        r = s + samples_per_class
+
+    '''
+    # eg: For 301 img samples/gesture for 4 gesture types
+    label[0:301]=0
+    label[301:602]=1
+    label[602:903]=2
+    label[903:]=3
+    '''
+
+    data, Label = shuffle(immatrix, label, random_state=2)
+    train_data = [data, Label]
+
+    (X, y) = (train_data[0], train_data[1])
+
+    # Split X and y into training and testing sets
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+
+    X_train = X_train.reshape(X_train.shape[0], img_channels, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], img_channels, img_rows, img_cols)
+
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+
+    # normalize
+    X_train /= 255
+    X_test /= 255
+
+    # convert class vectors to binary class matrices
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    return X_train, X_test, Y_train, Y_test
+
+
+def trainModel(model):
+
+    # Split X and y into training and testing sets
+    X_train, X_test, Y_train, Y_test = initializers()
+
+    # Now start the training of the loaded model
+    hist = model.fit(X_train, Y_train, batch_size=batch_size, epochs=nb_epoch,
+                 verbose=1, validation_split=0.2)
+
+    ##visualizeHis(hist)
+
+    ans = raw_input("Do you want to save the trained weights - y/n ?")
+    if ans == 'y':
+        filename = raw_input("Enter file name - ")
+        fname = path + str(filename) + ".hdf5"
+        model.save_weights(fname,overwrite=True)
+    else:
+        model.save_weights("newWeight.hdf5",overwrite=True)
+
+if __name__ == "__main__":
+    mod=loadCNN(-1, "")
+    trainModel(mod)
